f173da87fffabca8df6c7bddc6846c848bb4bf07
JackietBao <125871364+JackietBao@users.noreply.github.com>
2024-06-01 02:17:16 +0800

    PythonPython

--- Python/Ai人工智能算法工程师/2.Python入门/3.文件与文件夹操作/projectPython/main.py
+++ Python/Ai人工智能算法工程师/2.Python入门/3.文件与文件夹操作/projectPython/main.py
@@ -106,10 +106,15 @@ else:
 # 但由于出现意外，造成部分数据丢失，使得标注文件和图片文件的文件名前缀不能一一对应。
 # 需要写一段代码，将可以文件名前缀一一对应的文件保存到一个新的文件夹中，以完成数据的清洗。
 
-
 import os
 import shutil
-
+#例如这个文件的目录\\resources\\fire_yolo_format\\images\\test\\很多图片文件
+#1、file_i遍历test目录下图片的文件名，然后与目录"\\resources\\fire_yolo_format\\images\\test\\"
+#拼接到一起。最终形成例如：\\resources\\fire_yolo_format\\images\\test\\1.jpg
+#2、使用\\将这个目录结构分隔开：['resources', 'fire_yolo_format', 'images', 'test', '1.jpg']
+#3、列表的倒数第二个和最后一个元素拼接成一个新的路径。例如：\\test\\1.jpg
+#4、"."是为了分割成：['test\\1', 'jpg']得到一个新的列表
+#5、取这个列表的第一个元素 [0]，即 'test\\1'
 
 def get_info(root_from):
     file_full_path_list = []
@@ -128,13 +133,18 @@ def get_info(root_from):
 
     return file_full_path_list
 
-
+#清洗前的路径
 root_path_from = r'resources/fire_yolo_format'
+#清洗后的路径
 root_path_save = r'resources/clean_data'
 
+#拼接清洗前的路径+images=resources/fire_yolo_format/images
+#拼接清洗前的标签+labels=resources/fire_yolo_format/labels
 root_images_from = os.path.join(root_path_from, 'images')
 root_labels_from = os.path.join(root_path_from, 'labels')
 
+#拼接清洗后的路径+images=resources/fire_yolo_format/images
+#拼接清洗后的标签+labels=resources/fire_yolo_format/labels
 root_images_save = os.path.join(root_path_save, 'images')
 root_labels_save = os.path.join(root_path_save, 'labels')
 
@@ -146,19 +156,30 @@ print(root_labels_save)
 dir_list_1 = ['images', 'labels']
 dir_name_list = ['train','test', 'val']
 
+
+#创建新的目录/resources/clean_data/images/train
 for dir_1_i in dir_list_1:
     for dir_2_i in dir_name_list:
         dir_i_full_path = os.path.join(root_path_save, dir_1_i, dir_2_i)
         if not os.path.exists(dir_i_full_path):
             os.makedirs(dir_i_full_path)
 
+
+#调用get_info函数将拼接前的路径resources/fire_yolo_format/images
+#取短路径。例如：'test\1'
 image_full_path_list = get_info(root_images_from)
 print(image_full_path_list)
 
+#调用get_info函数将拼接前的路径resources/fire_yolo_format/labels
+#取短路径。例如：'test\1'
 label_full_path_list = get_info(root_labels_from)
 print(label_full_path_list)
 
 
+
+
+#取交集，图片名和文件名有一致性
+#例如：'test\1'
 image_set = set(image_full_path_list)
 label_set = set(label_full_path_list)
 
@@ -168,6 +189,8 @@ print(len(label_set))
 print(len(intersection_set))
 
 print(intersection_set)
+
+
 
 
 for intersection_i in intersection_set:
