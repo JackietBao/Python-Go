4e000c0fe8c3172618d903dc757e6d40403bdc01
JackietBao <125871364+JackietBao@users.noreply.github.com>
2024-06-09 20:21:44 +0800

    软件界面的操作

--- Python/Ai人工智能算法工程师/3.Python高级编程/6.软件界面的操作/projectPython/main.py
+++ Python/Ai人工智能算法工程师/3.Python高级编程/6.软件界面的操作/projectPython/main.py
@@ -1,6 +1,6 @@
 from QtTest import Ui_MainWindow
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
 from PyQt5 import QtGui
 
 class CamShow(QMainWindow, Ui_MainWindow):
@@ -9,13 +9,25 @@ class CamShow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         #信号与槽
         self.OpenFileBtn.clicked.connect(self.loadImage)
+        self.actionOpen.triggered.connect(self.loadImage)
+        self.actionexit.triggered.connect(self.exit)
+        self.actionabout.triggered.connect(self.about)
 
     def loadImage(self):
-        print("按钮被按下了")
+        #print("按钮被按下了")
+        #self.ifolabel.setText("打开文件按钮被按下了")
         self.fname, _ = QFileDialog.getOpenFileName(self, caption='选择图片', directory='.', filter='图像文件(*.jpg *.png)')
         print(self.fname)
         pix = QtGui.QPixmap(self.fname).scaled(self.ImageLabel.width(), self.ImageLabel.height())
         self.ImageLabel.setPixmap(pix)
+
+    def exit(self):
+        sys.exit(app.exec_())
+
+    def about(self):
+        self.ifolabel.setText("帮助文件按钮被按下了")
+        QMessageBox.information(self, '软件说明', '该软件由JackietBao制作而成，软件版本为 1.0')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
